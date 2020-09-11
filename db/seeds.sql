2955bfd925238e099d045b2c4816ed6d94fa996b
Jennifer Wong <jennifer.design@yahoo.com>
2020-09-10 22:16:36 -0500

    Created database files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) values ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) values ('Unroamed Burger', true);
+INSERT INTO burgers (burger_name, devoured) values ('Chick Fil A Burger', false);
+INSERT INTO burgers (burger_name, devoured) values ('Angus Beef Burger', false);
+INSERT INTO burgers (burger_name, devoured) values ('Beef-Fed Grass Burger', false);
