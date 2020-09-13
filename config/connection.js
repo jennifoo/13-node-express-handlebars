d15b308c7afc7acd5faac1d5290f55e16d44f317
Jennifer Wong <jennifer.design@yahoo.com>
2020-09-13 17:32:43 -0500

    fixed typo

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWS_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
@@ -12,8 +12,8 @@ if (process.env.JAWSDB_URL) {
     user: "root",
     password: "password",
     database: "burgers_db"
-  })
-}
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
