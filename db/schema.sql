68b646a089bb96129da8dcc6656a79651ad1117e
Jennifer Wong <jennifer.design@yahoo.com>
2020-09-13 17:24:04 -0500

    minor update to database files for Heroku launch

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,8 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255),
 	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
 );
-
-SELECT * FROM burgers;
